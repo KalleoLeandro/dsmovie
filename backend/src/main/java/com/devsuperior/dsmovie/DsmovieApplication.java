1b58d00c5e26ce1f9a8a45a50fef834b061aa077
Kalleo Leandro dos Santos Leal <kalleo.leal@gmail.com>
2022-05-11 10:04:51 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
@@ -3,10 +3,19 @@ package com.devsuperior.dsmovie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DsmovieApplication {
+/**
+ * Classe de execução do método principal
+ * @author Kalleo Leandro dos Santos Leal
+ * @since 11/05/2022
+ * @version 1.0
+ *
+ */
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class DsmovieApplication
+{
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(DsmovieApplication.class, args);
 	}
 
